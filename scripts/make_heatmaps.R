6dba06d4debee9ae2cf326a8deebec094554d270
Samuel Bunga <sambunga094@gmail.com>
2021-10-28 17:31:55 -0400

    added initial code to make heatmaps

---
+++ scripts/make_heatmaps.R
@@ -0,0 +1,59 @@
+library(xlsx)
+library(dplyr)
+library(tidyr)
+library(pheatmap)
+library(tidyverse)
+library(RColorBrewer)
+
+# set wd
+setwd('~/gitHub/panacea-rnaseq/woolf_sensory_ampliseq/')
+
+# functions
+get_colors <- function(){
+  my_colors = brewer.pal(n = 10, name = "RdBu")
+  my_colors = colorRampPalette(my_colors)(50)
+  my_colors = rev(my_colors)
+  return(my_colors)
+}
+
+draw_heatmap <- function(x, fname, height=2800,
+                         width=2500, res=250){
+  my_colors <- get_colors()
+  h <- pheatmap(x,
+                #scale = "row",
+                color = my_colors,
+                border_color = NA,
+                fontsize_row = 4, 
+                cluster_cols = F)
+  png(paste0('./output/', fname, '.png'), width = width,
+      height = height, res = res)
+  print(h)
+  dev.off()
+}
+
+# Read XLSX file
+ampli_df <- read.csv('./data/sensory_ampliseq.csv')
+ampli_df <- ampli_df[, c(1,3,4,5,6)]
+ampli_df <- ampli_df %>% column_to_rownames('Gene')
+
+# Read gene list
+gene_list <- read.csv('./data/gene_list.csv')
+
+png('./output/boxplot_raw.png', width=1500, height = 1500, res=150)
+print(boxplot(ampli_df))
+dev.off()
+
+ampli_log2 = log2(ampli_df + 1)
+png('./output/boxplot_log_2.png', width=1500, height = 1500, res=150)
+print(boxplot(ampli_log2))
+dev.off()
+
+genes_sub <- ampli_log2[rownames(ampli_log2) %in% gene_list$Genes,]
+genes_sub <- genes_sub[, c(4,2,3,1)]
+x <- t(scale(t(genes_sub)))
+x <- na.omit(x)
+draw_heatmap(x, 'all_samples',res=300, height = 1200, width=1000)
+
+x <- t(scale(t(genes_sub[, c(1,2,3)])))
+x <- na.omit(x)
+draw_heatmap(x, 'no_X5i.28d',res=300, height = 1200, width=1000)
