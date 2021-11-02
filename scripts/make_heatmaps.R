ad082e8ba04c40e2bbc4185d63d2aff84f39b501
Samuel Bunga <sambunga094@gmail.com>
2021-11-02 10:00:12 -0400

    gene annotations

--- scripts/make_heatmaps.R
+++ scripts/make_heatmaps.R
@@ -17,14 +17,16 @@ get_colors <- function(){
 }
 
 draw_heatmap <- function(x, fname, height=2800,
-                         width=2500, res=250){
+                         width=2500, res=250, anno = NULL){
   my_colors <- get_colors()
   h <- pheatmap(x,
                 #scale = "row",
                 color = my_colors,
                 border_color = NA,
                 fontsize_row = 4, 
-                cluster_cols = F)
+                cluster_cols = F,
+                annotation_row = anno,
+                cluster_rows = F)
   png(paste0('./output/', fname, '.png'), width = width,
       height = height, res = res)
   print(h)
@@ -37,8 +39,9 @@ ampli_df <- ampli_df[, c(1,3,4,5,6)]
 ampli_df <- ampli_df %>% column_to_rownames('Gene')
 
 # Read gene list
-gene_list <- read.csv('./data/gene_list.csv')
-
+#gene_list <- read.csv('./data/gene_list.csv')
+gene_list <- read.xlsx('./data/Gene list.xlsx', sheetIndex = 1)
+  
 png('./output/boxplot_raw.png', width=1500, height = 1500, res=150)
 print(boxplot(ampli_df))
 dev.off()
@@ -48,12 +51,19 @@ png('./output/boxplot_log_2.png', width=1500, height = 1500, res=150)
 print(boxplot(ampli_log2))
 dev.off()
 
-genes_sub <- ampli_log2[rownames(ampli_log2) %in% gene_list$Genes,]
+
+genes_sub <- ampli_log2[rownames(ampli_log2) %in% gene_list$Gene,]
 genes_sub <- genes_sub[, c(4,2,3,1)]
 x <- t(scale(t(genes_sub)))
 x <- na.omit(x)
-draw_heatmap(x, 'all_samples',res=300, height = 1200, width=1000)
+# Create annotation col
+anno <- gene_list[gene_list$Gene %in% rownames(x), ]
+rownames(anno) <- anno$Gene
+anno$Gene <- NULL
+x <- x[rownames(anno),]
+draw_heatmap(x, 'all_samples', anno = anno, res=300, height = 1200, width=1000)
 
 x <- t(scale(t(genes_sub[, c(1,2,3)])))
 x <- na.omit(x)
-draw_heatmap(x, 'no_X5i.28d',res=300, height = 1200, width=1000)
+x <- x[rownames(anno),]
+draw_heatmap(x, 'no_X5i.28d', anno = anno, res=300, height = 1200, width=1000)
