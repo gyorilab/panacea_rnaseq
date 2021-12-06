fd39cd093458d8783b212171a47668df154b0107
Samuel Bunga <sambunga094@gmail.com>
2021-12-06 17:12:06 -0500

    adding functions

---
+++ functions.R
@@ -0,0 +1,23 @@
+# functions
+get_colors <- function(){
+  my_colors = brewer.pal(n = 10, name = "RdBu")
+  my_colors = colorRampPalette(my_colors)(50)
+  my_colors = rev(my_colors)
+  return(my_colors)
+}
+
+draw_heatmap <- function(x, fname, height=2800,
+                         width=2500, res=250){
+  my_colors <- get_colors()
+  h <- pheatmap(x,
+                #scale = "row",
+                color = my_colors,
+                border_color = NA,
+                fontsize_row = 4, 
+                cluster_cols = T)
+  dir.create('./output', showWarnings = F)
+  png(paste0('./output/', fname, '.png'), width = width,
+      height = height, res = res)
+  print(h)
+  dev.off()
+}
