ef6f459ff80d5883e05420f2f1420b11689b07c3
Samuel Bunga <sambunga094@gmail.com>
2021-12-07 12:25:49 -0500

    add drg exc data

--- functions.R
+++ functions.R
@@ -6,15 +6,15 @@ get_colors <- function(){
   return(my_colors)
 }
 
-draw_heatmap <- function(x, fname, height=2800,
-                         width=2500, res=250){
+draw_heatmap <- function(x, fname, height=4800,
+                         width=2000, res=250){
   my_colors <- get_colors()
   h <- pheatmap(x,
                 #scale = "row",
                 color = my_colors,
                 border_color = NA,
                 fontsize_row = 4, 
-                cluster_cols = T)
+                cluster_cols = F)
   dir.create('./output', showWarnings = F)
   png(paste0('./output/', fname, '.png'), width = width,
       height = height, res = res)
--- generate_heatmaps.R
+++ generate_heatmaps.R
@@ -34,6 +34,7 @@ n_8_weeks <- n_8_weeks %>% dplyr::select(Human_symbol, Nociceptor_8Weeks_avg_tpm
 merged_df <- merge(h_drg, c(n_4_weeks), by='Human_symbol')
 merged_df <- merge(merged_df, c(n_8_weeks), by='Human_symbol')
 
+
 # Read ION channels
 ion_channel <- read.xlsx(paste0(wd, '/gene_lists/ion channel.xlsx'), sheetIndex = 1)
 ion_channel <- ion_channel %>% dplyr::select(ID,Symbol)
@@ -53,7 +54,61 @@ treg <- read.xlsx(paste0(wd, 'gene_lists/transcription regulator.xlsx'), sheetIn
 treg <- treg %>% dplyr::select(ID, Symbol)
 colnames(treg) <- c('ID', 'Human_symbol')
 
+# Immune related genes
+immu_genes <- read.xlsx(paste0(wd, 'gene_lists/immuerelatedgenes (12-06-21).xlsx'), sheetIndex = 1)
+immu_genes <- immu_genes %>% dplyr::select(ID, Symbol)
+colnames(immu_genes) <- c('ID', 'Human_symbol')
 
+# Tublin related genes
+tublin <- read.xlsx(paste0(wd, 'gene_lists/Tubilin linked genes (10-26-21).xlsx'), sheetIndex = 1)
+tublin <- tublin %>% dplyr::select(ID, Symbol)
+colnames(tublin) <- c('ID', 'Human_symbol')
+
+# common up-reg rags
+regs <- read.xlsx(paste0(wd, 'gene_lists/commonupregulatedRAGs in DRG.xlsx'), sheetIndex = 1)
+regs <- regs %>% dplyr::select(ID, Symbol)
+colnames(regs) <- c('ID', 'Human_symbol')
+
+# Drg exc
+drg_exc  <- read.xlsx(paste0(wd, 'gene_lists/EXtargets2021-07-17 2.0.xlsx'), sheetIndex = 1)
+drg_exc <- drg_exc %>% dplyr::select(Symbol, Entrez.Gene.Name)
+colnames(drg_exc)[1] <- c('Human_symbol')
+
+
+# Plot DRG exc
+merged_df_drg <- merged_df[merged_df$Human_symbol %in% drg_exc$Human_symbol, ]
+rownames(merged_df_drg) <- merged_df_drg$Human_symbol
+merged_df_drg$Human_symbol <- NULL
+x <- t(scale(t(merged_df_drg)))
+draw_heatmap(x, fname = 'drg_exc', res=300, height=4000)
+write.csv(merged_df_drg, './output/merged_df_drg_exc.csv')
+
+
+# Plot up-reg rags
+merged_df_regs <- merged_df[merged_df$Human_symbol %in% regs$Human_symbol, ]
+rownames(merged_df_regs) <- merged_df_regs$Human_symbol
+merged_df_regs$Human_symbol <- NULL
+x <- t(scale(t(merged_df_regs)))
+draw_heatmap(x, fname = 'up-reg_rags', res=300, height=8000)
+write.csv(merged_df_regs, './output/merged_df_regs.csv')
+
+
+# Plot tubulin related genes
+merged_df_tubulin <- merged_df[merged_df$Human_symbol %in% tublin$Human_symbol, ]
+rownames(merged_df_tubulin) <- merged_df_tubulin$Human_symbol
+merged_df_tubulin$Human_symbol <- NULL
+x <- t(scale(t(merged_df_tubulin)))
+draw_heatmap(x, fname = 'tubulin_linked_genes', res=300, width = 1000, height=2000)
+write.csv(merged_df_tubulin, './output/merged_df_tubulin.csv')
+
+
+# Plot immune related genes
+merged_df_immu <- merged_df[merged_df$Human_symbol %in% immu_genes$Human_symbol, ]
+rownames(merged_df_immu) <- merged_df_immu$Human_symbol
+merged_df_immu$Human_symbol <- NULL
+x <- t(scale(t(merged_df_immu)))
+draw_heatmap(x, fname = 'immu_related_genes', res=300, width = 1000, height=2000)
+write.csv(merged_df_immu, './output/merged_df_immune_related.csv')
 
 # Plot Ion channels
 merged_df_ion_c <- merged_df[merged_df$Human_symbol %in% ion_channel$Symbol, ]
@@ -61,6 +116,8 @@ rownames(merged_df_ion_c) <- merged_df_ion_c$Human_symbol
 merged_df_ion_c$Human_symbol <- NULL
 x <- t(scale(t(merged_df_ion_c)))
 draw_heatmap(x, fname = 'ion_channels', res=300, height = 6000)
+write.csv(merged_df_ion_c, './output/merged_df_ion_channels.csv')
+
 
 # Plot GPCR
 merged_df_gpcr <- merged_df[merged_df$Human_symbol %in% g_protein$Human_symbol, ]
@@ -68,6 +125,8 @@ rownames(merged_df_gpcr) <- merged_df_gpcr$Human_symbol
 merged_df_gpcr$Human_symbol <- NULL
 x <- t(scale(t(merged_df_gpcr)))
 draw_heatmap(x, fname = 'gpcrs')
+write.csv(merged_df_gpcr, './output/merged_df_gpcr.csv')
+
 
 # Plot Kinases
 merged_df_kinases <- merged_df[merged_df$Human_symbol %in% kinases$Human_symbol, ]
@@ -75,6 +134,7 @@ rownames(merged_df_kinases) <- merged_df_kinases$Human_symbol
 merged_df_kinases$Human_symbol <- NULL
 x <- t(scale(t(merged_df_kinases)))
 draw_heatmap(x, fname = 'kinases', res = 300, height = 10000)
+write.csv(merged_df_kinases, './output/merged_df_kinases.csv')
 
 
 merged_df_treg <- merged_df[merged_df$Human_symbol %in% treg$Human_symbol, ]
@@ -83,6 +143,50 @@ rownames(merged_df_treg) <- merged_df_treg$Human_symbol
 merged_df_treg$Human_symbol <- NULL
 x <- t(scale(t(merged_df_treg)))
 draw_heatmap(x, fname = 'treg', res = 300, height = 22000)
+write.csv(merged_df_treg, './output/merged_df_treg.csv')
+
+ion_channels <- list('Nav1.3' = 'SCN3A',
+                     'Nav1.7' = 'SCN9A',
+                     'Nav1.9' = 'SCN11A')
 
 
+ion_c_cols <- colnames(merged_df_ion_c)
 
+for(C in 1:length(ion_c_cols)){
+  cname <- str_replace(colnames(merged_df_ion_c)[C], '_avg_tpm', '')
+  for (I in names(ion_channels)) {
+    merged_df_ion_c[paste0(cname, '_', I)] <- NA
+  }
+  for(I in names(ion_channels)){
+    ion_value <- merged_df_ion_c[ion_channels[[I]], 
+                                 colnames(merged_df_ion_c)[C]]
+    for (r in 1:nrow(merged_df_ion_c)) {
+      diff = merged_df_ion_c[r, C]/ion_value
+      merged_df_ion_c[r,  paste0(cname, '_', I)] <- diff
+      }
+  }
+}
+write.csv(merged_df_ion_c, './output/merged_df_all_ion_channels.csv')
+
+# Make heatmaps for each channel
+# Nav1.3
+nav1.3_cols <- na.omit(str_extract(colnames(merged_df_ion_c), '.*1.3'))
+nav1.3_df <- merged_df_ion_c[, nav1.3_cols]
+x <- t(scale(t(nav1.3_df)))
+x <- na.omit(x)
+draw_heatmap(x, fname = 'nav1.3', res = 300, height = 9000)
+
+# Nav 1.7
+nav1.7_cols <- na.omit(str_extract(colnames(merged_df_ion_c), '.*1.7'))
+nav1.7_df <- merged_df_ion_c[, nav1.7_cols]
+x <- t(scale(t(nav1.7_df)))
+x <- na.omit(x)
+draw_heatmap(x, fname = 'nav1.7', res = 300, height = 9000)
+
+
+# Nav 1.9
+nav1.9_cols <- na.omit(str_extract(colnames(merged_df_ion_c), '.*1.9'))
+nav1.9_df <- merged_df_ion_c[, nav1.9_cols]
+x <- t(scale(t(nav1.9_df)))
+x <- na.omit(x)
+draw_heatmap(x, fname = 'nav1.9', res = 300, height = 9000)
